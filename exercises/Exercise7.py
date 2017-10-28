20f10660f7542e5b936f793e6f71ce0bb1288819
Philipp <siebesiech@gmail.com>
2017-10-28 20:49:57 +0200

    update

--- exercises/Exercise7.py
+++
@@ -1,9 +0,0 @@
-def print_reverse(text):
-    counter = len(text)-1
-    while counter >= 0:
-        print(text[counter])
-        counter -= 1
-
-
-string = input('String to reverse: ')
-print_reverse(string)
