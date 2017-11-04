baf4046d9b4b0cf1e757a7a21d5046aff5996df3
Philipp <siebesiech@gmail.com>
2017-11-04 08:11:31 +0100

    some examples

---
+++ custom exercises/Point.py
@@ -0,0 +1,19 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, p):
+        return Point(self.x+p.x, self.y+p.y)
+
+    def __str__(self):
+        return "x: " + str(self.x) + " y: " + str(self.y)
+
+
+
+p1 = Point(1,2)
+p2 = Point(2,3)
+
+p3 = p1 + p2
+
+print(p3)
---
+++ custom exercises/Test.py
@@ -0,0 +1,5 @@
+class Test():
+    def __init__(self):
+        self.x = 1
+
+
