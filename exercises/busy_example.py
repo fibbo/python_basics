15fbd29742778f7a33a1c0f729eddec8547e7e9e
Philipp <siebesiech@gmail.com>
2018-03-17 21:05:07 +0100

    Adding busy example

---
+++ exercises/busy_example.py
@@ -0,0 +1,29 @@
+def set_drawing(drawing):
+    global is_drawing # without this line any change to is_drawing has no effect to the is_drawing outside the function
+    is_drawing = drawing
+    isBusy()
+
+def set_saving(saving): 
+    global is_saving # without this line any change to is_saving has no effect to the is_saving outside the function
+    is_saving = saving
+    isBusy()
+
+
+def isBusy():
+    if (is_saving or is_drawing):
+        print("We are busy")
+    else:
+        print("Not busy")
+
+
+is_drawing = False
+is_saving = False
+
+
+set_saving(True)
+
+set_drawing(True)
+
+set_saving(False)
+
+set_drawing(False)
