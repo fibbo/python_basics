b0c94b1548584361a3e5c4f26ac8b9fa708e7ff1
Philipp <siebesiech@gmail.com>
2018-10-14 12:19:02 +0200

    ...

--- exercises/FizzBuzz.py
+++ exercises/FizzBuzz.py
@@ -1,6 +1,6 @@
 def fizz_buzz():
     for i in range(1, 101):
-        if (i % 3 == 0 and i % 5 == 0):
+        if i % 3 == 0 and i % 5 == 0:
             print("{} FizzBuzz".format(i))
         elif i % 5 == 0:
             print("{} Buzz".format(i))
