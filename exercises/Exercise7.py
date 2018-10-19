9186f037f94e5444212e784a22d04ff591952311
Philipp <siebesiech@gmail.com>
2018-10-19 21:04:22 +0200

    clean up 7

--- exercises/Exercise7.py
+++ exercises/Exercise7.py
@@ -3,18 +3,11 @@ def print_reverse(text):
     rev_string = ""
     while counter >= 0:
         rev_string += text[counter]
-        print(text[counter], end="") # end="" prevents a line break after a print
+        print(text[counter], end="")  # end="" prevents a line break after a print
         counter -= 1
 
     return rev_string
 
-<<<<<<< HEAD
 
-# string = input('String to reverse: ')
-print_reverse("asdfasdf")
-
-print(print_reverse("test"))
-=======
 string = input('String to reverse: ')
 print_reverse(string)
->>>>>>> b8b5b8a54481f5a2a4884a83874119d7d26b4fd9
