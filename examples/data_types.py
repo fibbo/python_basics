44f3c7fca7b54f1f305b9c33bdd8b2033ef5e461
Philipp <siebesiech@gmail.com>
2018-11-09 20:57:42 +0100

    examples, slightly updated presentation

---
+++ examples/data_types.py
@@ -0,0 +1,13 @@
+integer_variable = 73
+float_variable = 3.1415926
+string_variable = 'this is a string defined with single quotes'
+another_string_variable = "this is a string defined with double quotes'
+always_true = True
+always_false = False
+
+print(integer_variable)
+print(float_variable)
+print(string_variable)
+print(another_string_variable)
+print(always_true)
+print(always_false)
