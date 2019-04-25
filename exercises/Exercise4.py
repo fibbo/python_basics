ffc45f3b83821a462c5914ab5122049b202f55ce
Philipp <siebesiech@gmail.com>
2019-04-25 18:40:13 +0200

    Separate exercises and presentation

--- exercises/Exercise4.py
+++ exercises/Exercise4.py
@@ -1,13 +1,13 @@
 import math
 
 
-def volume_from_points(px1, py1, px2, py2):
-    radius = distance(px1, py1, px2, py1)
+def volume_from_points(px1, py1, pz1, px2, py2, pz2):
+    radius = distance(px1, py1, pz1, px2, py2, pz2)
     return volume_from_radius(radius)
 
 
-def distance(px1, py1, px2, py2):
-    return math.sqrt((px2 - px1) ** 2 + (py2 - py1) ** 2)
+def distance(px1, py1, pz1, px2, py2, pz2):
+    return math.sqrt((px2 - px1) ** 2 + (py2 - py1) ** 2 + (pz2 - pz1) ** 2)
 
 
 def volume_from_radius(radius):
@@ -16,7 +16,9 @@ def volume_from_radius(radius):
 
 x1 = int(input('Point 1 x-cord: '))
 y1 = int(input('Point 1 y-cord: '))
+z1 = int(input('Point 1 z-cord: '))
 x2 = int(input('Point 2 x-cord: '))
 y2 = int(input('Point 2 y-cord: '))
+z2 = int(input('Point 2 z-cord: '))
 
-print(volume_from_points(x1, y1, x2, y2))
+print(volume_from_points(x1, y1, z1, x2, y2, z2))
