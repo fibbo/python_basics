51867804d4c79d47b436788dd90bdaf7ac1867b3
Philipp <siebesiech@gmail.com>
2019-06-18 22:33:29 +0200

    exercise 1 reading data from file

---
+++ custom_exercises/Exercise1_File.py
@@ -0,0 +1,14 @@
+def compare(n1, n2):
+    if n1 > n2:
+        print(1)
+    elif n1 == n2:
+        print(0)
+    else:
+        print(-1)
+
+
+with open('custom_exercises/numbers.txt') as f:
+    data = f.read().split('\n')
+    for line in data:
+        split_line = line.split()
+        compare(int(split_line[0]), int(split_line[1]))
