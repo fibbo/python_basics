801187541401a22bbb6c052c1b5b51c89ac97346
IDKursleiter <idkursleiter@idy10e25mackl.uzh.ch>
2019-10-12 12:17:30 +0200

    allow repeating the guessing game

---
+++ exercises/exercise_10_repeatable.py
@@ -0,0 +1,27 @@
+import random
+
+def RunGame():
+    target = random.randrange(1, 101)
+
+    counter = 0
+
+    while True:
+        counter += 1
+        guess = float(input("Enter your guess: "))
+        if guess > target:
+            print("Your guess is too high.")
+        elif guess < target:
+            print("Your guess is too low")
+        else:
+            print("You got it! It took you {} tries".format(counter))
+            break
+
+
+RunGame()
+
+while True:
+    answer = input("Do you want to play again? ")
+    if answer == 'yes':
+        RunGame()
+    else:
+        break
