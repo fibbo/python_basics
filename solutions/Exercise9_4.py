479b6d4f07d900726cbf4b56788d9b8c8fdc334d
fibbo <siebesiech@gmail.com>
2021-04-21 22:36:59 +0200

    consistent rounding function

--- solutions/Exercise9_4.py
+++ solutions/Exercise9_4.py
@@ -1,7 +1,11 @@
+def round_to(n, precision):
+    correction = 0.5 if n >= 0 else -0.5
+    return int(n / precision + correction) * precision
+
+
 def calculate_mark(points, max_points):
     mark = float(points) * 5.0 / float(max_points) + 1
-    return round(mark * 2) / 2
-
+    return round_to(mark, 0.5)
 
 max_points = int(input('Enter maximum score\n'))
 
